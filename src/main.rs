8b0a1f36dcca54c37d671f30400d832a6f229b97
GodGotzi <eli.gottsbacher@gmail.com>
2022-12-17 17:34:46 +0100

    actix web framework

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,24 @@
-mod thread_pool;
+use actix_files::NamedFile;
+use actix_web::{HttpRequest, Result};
+use std::path::PathBuf;
 
-use crate::thread_pool::ThreadPool;
-use std::{fs};
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::io::prelude::*;
-use std::path::Path;
+async fn index(req: HttpRequest) -> Result<NamedFile> {
+    let str = match req.path() {
+         "/" => String::from("frontend/home.html"),
+         _ => format!("frontend{}", req.path()),
+    };
 
-fn main() {
-
-    let listener =
-        TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    let pool = ThreadPool::new(10);
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        pool.execute( || {
-           handle_connection(stream);
-        });
-    }
-
-    println!("Shutting down");
+    println!("Computing Path -> {}", str);
+    Ok(NamedFile::open(PathBuf::from(str))?)
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_web::{web, App, HttpServer};
 
-    let request_str = String::from_utf8_lossy(&buffer);
-    let request_args: Vec<&str> = request_str.split_whitespace().collect();
-    let path = format!("frontend{}", request_args.get(1).unwrap());
-
-    let (status_line, filepath) =
-        if Path::new(path.as_str()).exists() && Path::new(path.as_str()).is_file() {
-            ("HTTP/1.1 200 OK", path.as_str())
-        } else if Path::new(path.as_str()).is_dir() {
-            ("HTTP/1.1 200 OK", "frontend/home.html")
-        } else {
-            ("HTTP/1.1 200 OK", "frontend/err.html")
-        };
-
-    println!("{}", filepath);
-    let contents = fs::read_to_string(filepath).unwrap();
-
-    let response =
-    format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    HttpServer::new(|| App::new().route("/{filename:.*}", web::get().to(index)))
+        .workers(10)
+        .bind(("127.0.0.1", 7000))?
+        .run()
+        .await
 }
