b0a9b0c78f8e7fda2b5d2998eb9fb1976bad97e1
GodGotzi <eli.gottsbacher@gmail.com>
2022-12-19 15:43:13 +0100

    openssl ready

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,13 @@ use actix_web::{HttpRequest, Result};
 use std::path::PathBuf;
 use actix_web::http::header::ACCEPT_LANGUAGE;
 use actix_web::{web, App, HttpServer};
+use lazy_static::lazy_static;
+
+#[allow(unused_imports)]
+use openssl::ssl::{SslAcceptor, SslMethod, SslFiletype};
+
 use crate::language::{get_language, get_path, LanguageProperties};
 use crate::properties::Properties;
-use lazy_static::lazy_static;
 use crate::logging::{log_info, log_request, log_response, LogLevel};
 
 lazy_static! {
@@ -46,9 +50,17 @@ async fn main() -> std::io::Result<()> {
     log_info("Starting Webserver...", LogLevel::INFO);
     log_info("Waiting for Requests", LogLevel::INFO);
 
+    /*
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
+    */
+
     HttpServer::new(|| App::new().route("/{filename:.*}", web::get().to(index)))
         .workers(40)
-        .bind(("192.168.60.231", 3000))?
+        .bind(("172.20.10.2", 3000))? //172.20.10.2:3000
         .run()
         .await
 }
